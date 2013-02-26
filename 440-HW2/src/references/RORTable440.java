1a89aa3c7ddffd155bb65ab065b3323df77ceaa8
Justin Greet <Justin@Justin-PC.(none)>
2013-02-25 20:18:06 -0500

    Implement RORTable440.

--- 440-HW2/src/references/RORTable440.java
+++ 440-HW2/src/references/RORTable440.java
@@ -1,10 +1,14 @@
 package references;
 
+import java.util.HashMap;
+
 //This is simple. ROR needs a new object key for each remote object (or its skeleton). 
 //This can be done easily, for example by using a counter.
 //We also assume a remote object implements only one interface, which is a remote interface.
 
 public class RORTable440 {
+	HashMap<Integer, Object> hm = new HashMap<Integer, Object>();
+	private int nextKey = 0;
 	 // I omit all instance variables. you can use hash table, for example.
 	 // The table would have a key by ROR.
 	 
@@ -17,12 +21,18 @@ public class RORTable440 {
 	 // Using it, you can construct a ROR. 
 	 // The host and port are not used unless it is exported outside.
 	 // In any way, it is better to have it for uniformity.
-	 public void addObj(String host, int port, Object o) {
+	 public RemoteObjectReference addObj(String host, int port, Object o) {
+		 String[] interfaceNameComponents = o.getClass().getInterfaces()[0].getCanonicalName().split(".");
+		 String interfaceName = interfaceNameComponents[interfaceNameComponents.length - 1];
+		 RemoteObjectReference newROR = new RemoteObjectReference(host, port, nextKey, interfaceName);
+		 hm.put(nextKey, o);
+		 nextKey += 1;
+		 return newROR;
 	 }
 	
 	 // given ror, find the corresponding object.
-	 public Object findObj(RemoteObjectReference ror) {
+	 public Object findObj(int key) {
 		 // if you use a hash table this is easy.
-		 return null;
+		 return hm.get(key);
 	 }
 }
