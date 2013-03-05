e4ce0c05e1c04ef481bbbb3fc0da4d7347c3f725
Justin Greet <Justin@Justin-PC.(none)>
2013-03-05 01:57:41 -0500

    Changes

--- 440-HW2/src/examples/Compound.java
+++ 440-HW2/src/examples/Compound.java
@@ -7,7 +7,7 @@ public class Compound implements CompoundServer {
 	public GoodbyeServer compoundGoodbye() throws RemoteException440 {
 		GoodbyeServer gb = new Goodbye();
 		try {
-			gb.sayGoodbye();
+			System.out.println(gb.sayGoodbye());
 		} catch (RemoteException440 e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
